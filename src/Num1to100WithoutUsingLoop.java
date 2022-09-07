613c3efc8a0a5a6e79dc1aed90820759d231a5a5
aladnansami <aladnansami21@gmail.com>
2022-09-07 15:17:26 +0600

    first commit

---
+++ src/Num1to100WithoutUsingLoop.java
@@ -0,0 +1,26 @@
+package interviewprograms.src;
+
+/**
+ *
+ * @author al adnan sami
+ */
+
+
+public class Num1to100WithoutUsingLoop {
+    public static void main(String[] args) {
+        printNumber(1);
+    }
+    
+    public static void printNumber(int n){
+        if(n<=10){
+            System.out.print(n+"\t");
+            printNumber(n+1);
+        }
+    }    
+}
+/*
+                run:
+        1	2	3	4	5	6	7	8	9	10	
+ */
+
+
---
+++ src/OddEven.java
@@ -0,0 +1,40 @@
+package interviewprograms.src;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ *
+ * @author al adnan sami
+ */
+
+
+public class OddEven {
+    public static void main(String[] args) {
+        
+        //BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        //Integer in= Integer.parseInt(br.readLine());    //Another way to take input
+        
+        System.out.println("Enter number to check: ");
+        Scanner sc= new Scanner(System.in);
+        int in= sc.nextInt();
+        
+        if(in%2==0){
+            System.out.println("Number is even");
+        }
+        else{
+            System.out.println("Number is odd");
+        }
+    }
+}
+
+/*
+    Enter number to check: 
+    78
+    Number is even
+
+    Enter number to check: 
+    19
+    Number is odd
+ */
